ab9ecf5a12b83a274a254487a65f576472314f85
sga <sgallen@gmail.com>
2015-06-02 20:10:20 -0400

    Connects Models to View and Templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category, Page
+
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage': 'I am bold font from the context', }
+    top_pages = Page.objects.order_by('-views')[:5];
+    category_list = Category.objects.order_by('-likes')[:5];
+    context_dict = {'top_pages': top_pages, 'categories': category_list, }
     return render(request, 'rango/index.html', context_dict)
+
+def category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
+
 
 def about(request):
     return render(request, 'rango/about.html')
